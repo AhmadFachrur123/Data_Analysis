7161dcabbeb9e8f195cfa6f718fa302f17ea51c6
AhmadFachrur123 <101375578+AhmadFachrur123@users.noreply.github.com>
2022-03-10 19:45:12 -0800

    Update SQL_Statement.sql

--- SQL_Statement.sql
+++ SQL_Statement.sql
@@ -1,2 +1,33 @@
 == Mengambil Seluruh Kolom
 SELECT * FROM ms_produk;
+
+== Mengambil Satu Kolom dari Tabel
+SELECT nama_produk FROM ms_produk;
+
+== Mengambil Lebih dari Satu Kolom dari Tabel
+SELECT nama_produk,harga FROM ms_produk;
+
+== Membatasi Pengambilan Jumlah Row Data
+SELECT nama_produk, harga FROM ms_produk LIMIT 5;
+
+== Penggunaan SELECT DISTINCT statement
+SELECT DISTINCT(nama_customer), alamat FROM ms_pelanggan;
+
+== Menggunakan Prefix pada Nama Kolom
+SELECT ms_produk.kode_produk FROM ms_produk
+
+== Menggunakan Alias pada Kolom
+SELECT no_urut AS nomor, nama_produk AS nama FROM ms_produk
+
+== Menghilangkan Keyword 'AS'
+SELECT no_urut nomor, nama_produk nama FROM ms_produk
+
+== Menggabungkan Prefix dan Alias
+SELECT ms_produk.harga AS harga_jual FROM ms_produk
+
+== Menggunakan Alias pada Tabel
+SELECT * FROM ms_produk t2
+
+== Prefix dengan Alias Tabel
+SELECT t2.nama_produk,t2.harga FROM ms_produk t2;
+
