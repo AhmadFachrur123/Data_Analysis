d722702aa3da049ec4b8243b0cb6928702151a05
AhmadFachrur123 <101375578+AhmadFachrur123@users.noreply.github.com>
2022-04-02 22:29:13 -0700

    Update Project Data Analysis for B2B Retail: Customer Analytics Report.sql

--- Project Data Analysis for B2B Retail: Customer Analytics Report.sql
+++ Project Data Analysis for B2B Retail: Customer Analytics Report.sql
@@ -45,4 +45,4 @@ ORDER BY total_order DESC;
 -- Seberapa banyak customers yang tetap aktif bertransaksi setelah transaksi pertamanya
 SELECT COUNT(DISTINCT customerID) AS total_customers FROM orders_1;
 SELECT '1' AS quarter, (COUNT(DISTINCT customerID) * 100) / 25  AS Q2 FROM orders_1
-WHERE customerID IN (SELECT DISTINCT(customerID) FROM orders_2)
+WHERE customerID IN (SELECT DISTINCT(customerID) FROM orders_2);
